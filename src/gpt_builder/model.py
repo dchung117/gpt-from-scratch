fd05c23b170e6800c6209597e3de29c34ab958d8
dchung117 <dchung0330@gmail.com>
2023-11-16 21:47:48 +0000

    created generate function for llm to append new tokens to given context.

--- src/gpt_builder/model.py
+++ src/gpt_builder/model.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class BigramLLM(nn.Module):
     """
-    Bigram large language model.
+    Bigram large language model. Maps each input token to logits for each element in vocabulary.
 
     Args
     ----
@@ -17,3 +18,30 @@ class BigramLLM(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x_embed = self.embedding(x)
         return x_embed
+
+    def generate(self, x: torch.Tensor, n_gen: int) -> torch.Tensor:
+        """
+        Generate new tokens for given input.
+
+        Args
+        ----
+            x: torch.Tensor
+                Context token sequence (shape: (*, vocab_size))
+            n_gen: int
+                Number of new tokens to generate
+        Return
+        ------
+            torch.Tensor:
+                Sequence containing input context and generated tokens
+        """
+        if len(x.shape) == 1: # Unsqueeze to b, t
+            x = x.unsqueeze(0)
+
+        for _ in range(n_gen):
+            logits = self(x)[:, -1, :] # Only need to get predictions for last token
+            probs = F.softmax(logits, dim=-1)
+
+            x_next = torch.multinomial(probs, num_samples=1)
+            x = torch.cat((x, x_next), dim=1)
+
+        return x
