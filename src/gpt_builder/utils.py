9cbf90569c1abe4b4ea408eef1cf39dd422b82b4
dchung117 <dchung0330@gmail.com>
2023-12-17 02:06:26 +0000

    wrote evaluation functinos.

--- src/gpt_builder/utils.py
+++ src/gpt_builder/utils.py
@@ -1,5 +1,7 @@
 import torch
+from torch import nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
 
 def bigram_crossentropy_loss(preds: torch.Tensor, tgts: torch.Tensor) -> torch.Tensor:
     """
@@ -53,3 +55,34 @@ def train_step(model: torch.nn.Module,
     optimizer.step()
 
     return loss.item()
+
+def get_loss(dl: DataLoader,
+    model: nn.Module,
+    device: torch.device
+    ) -> float:
+    """
+    Estimate loss.
+
+    Args
+    ----
+        dl: DataLoader
+            Dataloader containing bigrams
+        model: nn.Module
+            Large language model
+        device: torch.device
+            Model device
+    
+    Return
+    ------
+        float: 
+            Loss
+    """
+    losses = []
+    model.eval()
+    with torch.inference_mode():
+        for inputs, targets in dl:
+            logits = model(inputs.to(device)).cpu()
+            loss = bigram_crossentropy_loss(logits, targets).item()
+            losses.append(loss)
+
+    return sum(losses)/len(losses)
