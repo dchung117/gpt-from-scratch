fc0d5d31786bb42a21302369e18ce5cff7bad003
dchung117 <dchung0330@gmail.com>
2023-12-29 02:47:58 +0000

    wrote attention head module and forward pass for gpt llm.

--- src/gpt_builder/model/gpt.py
+++ src/gpt_builder/model/gpt.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class GPTLanguageModel(nn.Module):
     """
@@ -17,15 +18,17 @@ class GPTLanguageModel(nn.Module):
         Number of attention heads per decoder
     d_embed: int (def. 384)
         Dimensionality of embeddings
+    p_dropout: float (def. 0.2)
+        Dropout hyperparameter
     """
     def __init__(self, vocab_size: int, block_size: int,
-        n_decoders: int = 4, n_heads: int = 8, d_embed: int = 384) -> None:
+        n_decoders: int = 4, n_heads: int = 8, d_embed: int = 38, p_dropout: float = 0.2) -> None:
         super().__init__()
         self.block_size = block_size
         self.token_embedding = nn.Embedding(vocab_size, d_embed)
         self.pos_embedding = nn.Embedding(block_size, d_embed)
         self.decoders = nn.Sequential(
-            *[DecoderBlock(d_embed, n_heads) for _ in range(n_decoders)]
+            *[DecoderBlock(d_embed, n_heads, block_size, p_dropout=p_dropout) for _ in range(n_decoders)]
         )
         self.layer_norm = nn.LayerNorm(d_embed)
         self.head = nn.Linear(d_embed, vocab_size)
@@ -70,12 +73,16 @@ class DecoderBlock(nn.Module):
             Dimensionality of embeddings
         n_heads: int
             Number of attention heads
+        block_size: int
+            Number of tokens per example
+        p_dropout: float (def. 0.2)
+            Dropout hyperparameter
     """
-    def __init__(self, d_embed: int, n_heads: int) -> None:
+    def __init__(self, d_embed: int, n_heads: int, block_size: int, p_dropout: float = 0.2) -> None:
         super().__init__()
         d_head = d_embed // n_heads   # dimensionality for each head
-        self.attn = MultiHeadAttention(n_heads, d_head, d_embed)
-        self.ff = FeedForward(d_embed)
+        self.attn = MultiHeadAttention(n_heads, block_size, d_head, d_embed, p_dropout=p_dropout)
+        self.ff = FeedForward(d_embed, p_dropout=p_dropout)
         self.layer_norm_1 = nn.LayerNorm(d_embed)
         self.layer_norm_2 = nn.LayerNorm(d_embed)
 
@@ -96,6 +103,8 @@ class MultiHeadAttention(nn.Module):
     ----
         n_heads: int
             Number of attention heads
+        block_size: int
+            Number of tokens per example
         d_head: int
             Dimensionality of each attention head
         d_embed: int
@@ -103,10 +112,11 @@ class MultiHeadAttention(nn.Module):
         p_dropout: float (def. 0.2)
             Dropout hyperparameter
     """
-    def __init__(self, n_heads: int, d_head: int, d_embed: int, p_dropout: float = 0.2) -> None:
+    def __init__(self, n_heads: int, block_size: int,
+        d_head: int, d_embed: int, p_dropout: float = 0.2) -> None:
         super().__init__()
         self.heads = nn.ModuleList(
-            [AttentionHead(d_head) for _ in range(n_heads)]
+            [AttentionHead(d_head, d_embed, block_size, p_dropout) for _ in range(n_heads)]
         )
         self.ff = nn.Linear(n_heads * d_head, d_embed)
         self.dropout = nn.Dropout(p=p_dropout)
@@ -121,9 +131,40 @@ class MultiHeadAttention(nn.Module):
 class AttentionHead(nn.Module):
     """
     Attention head module for GPT LLM
-    """
-    pass
 
+    Args
+    ----
+        d_head: int
+            Dimensionality of attention head
+        d_embed: int
+            Dimensionality of embeddings
+        block_size: int
+            Number of tokens per example
+        p_dropout: float (def. 0.2)
+            Dropout hyperparameter
+    """
+    def __init__(self, d_head: int, d_embed: int, block_size: int, p_dropout: float = 0.2) -> None:
+        super().__init__()
+        self.query = nn.Linear(d_embed, d_head, bias=False)
+        self.key = nn.Linear(d_embed, d_head, bias=False)
+        self.value = nn.Linear(d_embed, d_head, bias=False)
+
+        #  saving lower triangular mask (buffer is not a parameter)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))  
+        self.dropout = nn.Dropout(p_dropout)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        T = x.shape[-2]  # (B, T, C)
+        q, k, v = self.query(x), self.key(x), self.value(x)
+    
+        #  attention
+        wts = q @ k.transpose(-2, -1) * (k.shape[-1] ** -0.5)  #  (B, T, d_head) x (B, d_head, T) -> (B, T, T)
+        wts = wts.masked_fill(self.tril[:T, :T] == 0, float("-inf"))  # future tokens cannot attend to past
+        wts = F.softmax(wts, dim=-1)
+        wts = self.dropout(wts)
+
+        return wts @ v  # (B, T, T) x (B, T, d_head) -> (B, T, d_head)
+    
 class FeedForward(nn.Module):
     """
     Feedforward GPT LLM module
